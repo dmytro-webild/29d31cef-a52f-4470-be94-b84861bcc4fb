188eee8eba404e46f77aac33a21adfe95284405a
kudinDmitriyUp <kudin.dmitriy.up@gmail.com>
2026-06-26 18:09:41 +0000

    Bob AI: Add contact page

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import { Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import HomePage from './pages/HomePage';
 
+import ContactPage from "@/pages/ContactPage";
 export default function App() {
   return (
     <Routes>
       <Route element={<Layout />}>
         <Route path="/" element={<HomePage />} />
+        <Route path="/contact" element={<ContactPage />} />
       </Route>
     </Routes>
   );
