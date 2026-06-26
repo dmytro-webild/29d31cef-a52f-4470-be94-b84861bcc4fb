e2b579169d7ba90ca76012fa8c7d9e7fe5f57988
bender <kudin.dima@gmail.com>
2026-06-26 17:41:00 +0000

    Switch to version 6: modified src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-I mport { Routes, Route } from 'react-router-dom';
-I mport Layout from './components/Layout';
-I mport HomePage from './pages/HomePage';
+import { Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import HomePage from './pages/HomePage';
 
-export default functI on App() {
+export default function App() {
   return (
     <Routes>
       <Route element={<Layout />}>
