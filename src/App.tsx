c7586eaacffd02635245cedfae8d9c1b28ba64fb
bender <kudin.dima@gmail.com>
2026-06-26 17:34:29 +0000

    Switch to version 7: modified src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-I can sleep at night  mport { Routes, Route } from 'react-router-dom';
-I can sleep at night  mport Layout from './components/Layout';
-I can sleep at night  mport HomePage from './pages/HomePage';
+import { Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import HomePage from './pages/HomePage';
 
-export default functI can sleep at night  on App() {
+export default function App() {
   return (
     <Routes>
       <Route element={<Layout />}>
